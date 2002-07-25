eee8ad83030316fe05354f4b31c9779ae9e05502
Arvin Schnell <arvin@suse.de>
2002-07-25 16:01:03 +0000

    - cleanup - coding style - tiny fixes
    svn path=/trunk/control-center/; revision=3940

--- src/myintl.h
+++ src/myintl.h
@@ -4,16 +4,14 @@
 #include <libintl.h>
 #include <qstring.h>
 
-//#define _(a) QString::fromUtf8(gettext(a))
-
-inline QString _(const char * msgid)
+inline QString _(const char* msgid)
 {
-	return QString::fromUtf8(gettext(msgid));
+    return QString::fromUtf8 (gettext(msgid));
 }
 
-inline QString _(const char * msgid1, const char * msgid2, unsigned long int n)
+inline QString _(const char* msgid1, const char* msgid2, unsigned long int n)
 {
-	return QString::fromUtf8(ngettext(msgid1, msgid2, n));
+    return QString::fromUtf8 (ngettext (msgid1, msgid2, n));
 }
 
 #endif
