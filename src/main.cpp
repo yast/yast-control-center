2c7ed6a798e4cca6cc622bc66144daee01f57dcc
Katarína Machálková <kmachalkova@suse.cz>
2009-07-08 16:24:55 +0000

    Process CLI options ( --help only so far)
    svn path=/trunk/control-center/; revision=57955

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,51 @@
 #include <QApplication>
 #include <QTimer>
 
+#include <iostream>
+
+using std::cout;
+using std::cerr;
+using std::endl;
+
 #include "main_window.h"
 
+void printhelpandexit()
+{
+    cout << "Usage:"
+	 << "\n   y2controlcenter [OPTIONS]"
+	 << "\n"
+	 << "\nOPTIONS:"
+	 << "\n"
+	 << "\n    --help         -h    this message"
+	 << "\n    --root         -r    also show \"root only\" modules"
+	 << "\n    --fullscreen         use full screen"
+	 << "\n    --noborder           no window manager border for main window"
+	 << endl;
+    exit (0);
+}
 
 int main(int argc, char **argv)
 {
-QApplication app(argc, argv);
+    QApplication app(argc, argv);
 
-MainWindow mainWin;
+    for ( int i=0 ;i < argc ;i++ )
+    {
+	QString opt = argv[i];
 
-mainWin.show();
+	// Normalize command line option - accept "--xy" as well as "-xy"
 
-QTimer::singleShot(60, &mainWin, SLOT( initialMsg() ));
+	if ( opt.startsWith( "--" ) )
+	    opt.remove(0, 1);
 
-return app.exec();
+	if ( opt == "-h" || opt == "-help" )	printhelpandexit();
+    }
+	   
+    MainWindow mainWin;
+    
+    mainWin.show();
+    
+    QTimer::singleShot(60, &mainWin, SLOT( initialMsg() ));
+    
+    return app.exec();
 
 }
